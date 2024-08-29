f765d03d6dfe2bcb4935cb7758e14982f982f916
Jon <jdbaarsch@gmail.com>
2024-08-29 07:05:58 -0500

    Add JLabelDemo and TextFieldDemo

---
+++ GUI/src/JLabelDemo.java
@@ -0,0 +1,62 @@
+import java.awt.*;
+import javax.swing.*;
+
+public class JLabelDemo {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("JLabel Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(new JLabelPanel());
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private static class JLabelPanel extends JPanel {
+        public JLabelPanel(){
+            this.setPreferredSize(new Dimension(700, 700));
+            this.setBackground(Color.WHITE);
+
+            JLabel label = new JLabel("Hello World");
+
+            label.setFont(new Font("Serif", Font.BOLD, 50));
+            label.setForeground(Color.BLUE);
+            //label.setBackground(Color.magenta);
+            //label.setOpaque(true);
+
+            String text = label.getText();
+            label.setText("Good Morning");
+
+            //this.add(label);
+
+            ImageIcon img = new ImageIcon("images/penny.jfif");
+
+            JLabel imgLabel = new JLabel(img);
+            this.add(imgLabel);
+            //label.setIcon(img);
+            //label.setHorizontalTextPosition(SwingConstants.LEFT);
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+}
---
+++ GUI/src/TextFieldDemo.java
@@ -0,0 +1,72 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextFieldDemo {
+
+    public static void main(String[] args) {
+
+        JFrame frame = new JFrame("TextField Demo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        frame.getContentPane().add(new TextFieldPanel());
+
+        frame.pack();
+        frame.setVisible(true);
+
+
+    }
+
+    public static class TextFieldPanel extends JPanel {
+
+        private JTextField textField;
+
+        private JLabel label;
+
+
+        public TextFieldPanel() {
+            this.setPreferredSize(new Dimension(700, 700));
+            this.setBackground(Color.WHITE);
+
+            textField = new JTextField();
+            textField.setPreferredSize(new Dimension(300, 50));
+            textField.setFont(new Font("sansserif", Font.PLAIN, 40));
+            textField.addActionListener(new CoinListener());
+
+            label = new JLabel("Nothing yet");
+            label.setFont(new Font("sansserif", Font.PLAIN, 40));
+
+            this.add(textField);
+            this.add(label);
+        }
+
+        private class CoinListener implements ActionListener {
+
+            public void actionPerformed(ActionEvent e) {
+                //label.setText("Something Happened!");
+
+                String text = textField.getText();
+                double amt = Double.parseDouble(text);
+
+                System.out.println(text);
+
+
+                label.setText("$" + amt);
+
+                label.setIcon(new ImageIcon("images/penny.jfif"));
+
+
+            }
+
+        }
+
+
+
+
+    }
+
+
+
+
+}
