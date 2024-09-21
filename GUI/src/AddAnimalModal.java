300c5fa63c4539e28da7c99279b96b41347dad66
Jon <jdbaarsch@gmail.com>
2024-09-21 10:21:17 -0500

    WIP: Adjust addAnimalModal to accept different Animals.

--- GUI/src/AddAnimalModal.java
+++ GUI/src/AddAnimalModal.java
@@ -2,12 +2,19 @@ import Interfaces.Animal;
 import Interfaces.Dog;
 
 import javax.swing.*;
+import javax.swing.text.JTextComponent;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class AddAnimalModal extends JFrame {
 
     private AnimalListPanel animalListPanel;
     AddAnimalModal modal;
+    // passes the list of strings into an abstract List, which is then used to build a concrete ArrayList.
+    // Represents the different types of Animals I want to create.
+    public static final ArrayList<String> animalTypes =  new ArrayList<>(Arrays.asList("Dog", "Rooster", "Cat"));
 
     public AddAnimalModal(AnimalListPanel animalListPanel) {
         modal = this;
@@ -16,7 +23,6 @@ public class AddAnimalModal extends JFrame {
         this.setTitle("Add Animal");
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.getContentPane().add(new AddAnimalPanel());
-
         this.pack();
         this.setVisible(true);
 
@@ -24,7 +30,45 @@ public class AddAnimalModal extends JFrame {
 
     private class AddAnimalPanel extends JPanel {
 
+        private record Attribute( String name, JComponent value){};
+
+        private ArrayList<Attribute> attributes;
+
         public AddAnimalPanel (){
+            attributes = new ArrayList<>();
+            JPanel infoCollectorPanel = new JPanel();
+            infoCollectorPanel.setPreferredSize(new Dimension(600, 400));
+            infoCollectorPanel.setBackground(Color.magenta);
+
+            JComboBox<String> animalTypeComboBox = new JComboBox<String>(animalTypes);
+            animalTypeComboBox.addActionListener(e -> {
+                attributes.clear();
+                infoCollectorPanel.removeAll();
+                switch (animalTypeComboBox.getSelectedIndex()) {
+                    case : {
+                        attributes.add(new Attribute("Name", new JTextField(10)));
+                        attributes.add(new Attribute("Age", new JTextField(10)));
+                        attributes.add(new Attribute("Color", new JColorChooser(Color.WHITE)));
+                        attributes.add(new Attribute("Size", new JComboBox<Dog.DogSize>(Dog.DogSize.values())));
+                    }
+                    case 1: {
+                        attributes.add(new Attribute("Name", new JTextField(10)));
+                    }
+
+
+
+
+
+
+
+
+                }
+
+                infoCollectorPanel.add(new Attribute(new JLabel("Name"), new JTextField(10)))
+
+            });
+
+
             JTextField nameField = new JTextField(10);
             JButton addAnimalButton = new JButton("Submit");
             addAnimalButton.addActionListener(e -> {
@@ -36,6 +80,8 @@ public class AddAnimalModal extends JFrame {
             this.add(nameField);
             this.add(addAnimalButton);
         }
+
+
 
 
 
